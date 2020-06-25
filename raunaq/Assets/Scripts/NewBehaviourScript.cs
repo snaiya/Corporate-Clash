3975992b0aada776339a42461c0f17ba7d36bd09
Raunaq Ramesh Porwal <66442906+raunaqrameshporwal@users.noreply.github.com>
2020-06-25 15:26:36 -0700

    Sales Report Data

---
+++ raunaq/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // private Vector3 screenBounds;
+    public GameObject consumer;
+    public float time=3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameObject board = GameObject.Find("Tiles");
+        for(int i =0 ; i<15;i++){
+             GameObject c = Instantiate(consumer) as GameObject;
+             c.transform.position = new Vector2(Random.Range(-45.0f,40.0f),Random.Range(-39.0f,33.0f));
+        }
+		clusters c1 = new clusters();
+		c1.assign_bots_to_area();
+		variable.updateBuildingCount();
+		salesreport.countBuildingTypes();
+        
+
+    }
+    private void spawnConsumer(){
+       
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+         
+        }
+}
+
---
+++ raunaq/Assets/Scripts/variable.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * THIS IS A MASTER VARIABLES SCRIPT. ONLY FETCH DATA FROM THESE
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variable : MonoBehaviour
+{
+    // Main mapping of which tiles belong to which area
+    public static Dictionary<string, List<GameObject>> Mapping = new Dictionary<string, List<GameObject>>();
+
+    //Mapping of which tile belong to which player in which area
+    public static Dictionary<string, List<GameObject>> Player1 = new Dictionary<string, List<GameObject>>();
+    public static Dictionary<string, List<GameObject>> Player2 = new Dictionary<string, List<GameObject>>();
+    //public static Dictionary<string, List<GameObject>> Assigned_Street = new Dictionary<string, List<GameObject>>();
+    // Start is called before the first frame update
+
+    public static Dictionary<string, List<int>> Land = new Dictionary<string, List<int>>();
+
+    public static Dictionary<int, int> Luxury_cluster = new Dictionary<int, int>();
+    public static Dictionary<int, int> Alleyway_cluster = new Dictionary<int, int>();
+    public static Dictionary<int, int> Street_cluster = new Dictionary<int, int>();
+
+    public static Dictionary<string, List<GameObject>> Alleyway_building = new Dictionary<string, List<GameObject>>();
+    public static Dictionary<string, List<GameObject>> Luxury_building = new Dictionary<string, List<GameObject>>();
+    public static Dictionary<string, List<GameObject>> Street_building = new Dictionary<string, List<GameObject>>();
+	
+	public static Dictionary<string, Dictionary<string, List<GameObject>>> BuildingDict= new  Dictionary<string, Dictionary<string, List<GameObject>>>(){{"Luxury", Luxury_building}, {"Alleyway", Alleyway_building}
+	,{"Street", Street_building}};
+
+
+    
+
+    public static void updateBuildingCount()
+	{
+		BuildingDict["Luxury"] = Luxury_building;
+		BuildingDict["Alleyway"] = Alleyway_building;
+		BuildingDict["Street"] = Street_building;
+		
+	}
+	void Start()
+    {
+        // sample s1 = new sample();
+        // s1.New();
+
+	
+        //foreach (KeyValuePair<int, int> kvp in Alleyway_cluster){
+             //Debug.Log(kvp.Key);
+	//Debug.Log(kvp.Value);
+             //foreach (GameObject g in kvp.Value) {
+                 //Debug.Log(g.name);
+                
+             //}
+            
+        }
+        //Debug.Log(Mapping);
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
